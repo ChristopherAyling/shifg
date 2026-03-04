aa05343f41671354cc5c181dc04de15d9fe76eba
Chris Ayling <chris.ayling.360@gmail.com>
2026-03-04 11:44:18 +1000

    complete placement menu

--- src/editor.zig
+++ src/editor.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const assert = std.debug.assert;
-const api = @import("game_api.zig");
+const api = @import("editor_api.zig");
 const draw = @import("draw.zig");
 const ui = @import("ui.zig");
 const Image = @import("image.zig").Image;
@@ -13,25 +13,44 @@ const con = @import("constants.zig");
 const effects = @import("effects.zig");
 const Level = @import("level.zig").Level;
 const ThingPool = @import("things.zig").ThingPool;
+const Kind = @import("things.zig").Kind;
 const menus = @import("menus.zig");
 const Inputs = control.Inputs;
 const render_shared = @import("render_shared.zig");
 
+const PlacementEntry = struct { label: []const u8, icon: sprites.SpriteKey };
+const PLACEMENT_MENU_DATA = [_][]const PlacementEntry{
+    // npcs
+    &.{
+        .{ .label = "Argaven", .icon = .argaven },
+        .{ .label = "Estraven", .icon = .estraven },
+    },
+    // items
+    &.{
+        .{ .label = "potion", .icon = .potion },
+        .{ .label = "flag", .icon = .redflag },
+    },
+    // player
+    &.{
+        .{ .label = "player", .icon = .genly },
+    },
+};
+
 fn make_placement_menu() menus.NamedItemListCollection {
     var npcs = menus.NamedItemList.init("npc");
-    npcs.add("aaaa", null);
-    npcs.add("bbb", null);
-    npcs.add("Argaven", .argaven);
-    npcs.add("ccccccccc", null);
-    npcs.add("ccccccccc", null);
-    npcs.add("cc", null);
+    for (PLACEMENT_MENU_DATA[0]) |entry| {
+        npcs.add(entry.label, entry.icon);
+    }
 
     var items = menus.NamedItemList.init("items");
-    items.add("potato", null);
-    items.add("rabbit", null);
+    for (PLACEMENT_MENU_DATA[1]) |entry| {
+        items.add(entry.label, entry.icon);
+    }
 
     var player = menus.NamedItemList.init("player");
-    player.add("genly", null);
+    for (PLACEMENT_MENU_DATA[2]) |entry| {
+        player.add(entry.label, entry.icon);
+    }
 
     var placement_menu = menus.NamedItemListCollection.init();
     placement_menu.add(npcs);
@@ -40,8 +59,25 @@ fn make_placement_menu() menus.NamedItemListCollection {
 
     return placement_menu;
 }
+const PLACEMENT_MENU = make_placement_menu();
 
-pub fn editor_step(memory: *api.GameMemory, inputs: *const Inputs, platform_api: *const api.PlatformAPI) callconv(.c) void {
+fn place(things: *ThingPool, x: i32, y: i32, category: usize, index: usize) void {
+    const entry = PLACEMENT_MENU_DATA[category][index];
+    switch (category) {
+        0 => {
+            _ = things.add_npc(entry.icon, x, y);
+        },
+        1 => {
+            _ = things.add_item(entry.icon, x, y);
+        },
+        2 => {
+            _ = things.add_player(entry.icon, x, y);
+        },
+        else => unreachable,
+    }
+}
+
+pub fn editor_step(memory: *api.EditorMemory, inputs: *const Inputs, platform_api: *const api.PlatformAPI) callconv(.c) void {
     _ = platform_api;
     const editor_state = memory.state;
 
@@ -62,8 +98,10 @@ pub fn editor_step(memory: *api.GameMemory, inputs: *const Inputs, platform_api:
                 },
                 .editor_place => |*editor_place_menu| {
                     if (inputs.a.pressed) {
-                        // TODO place in the world
-
+                        // TODO get x and y from selector
+                        place(&editor_state.things, 0, 0, editor_place_menu.category, editor_place_menu.index);
+                        editor_state.menu.pop();
+                        return;
                     }
                     if (inputs.left.pressed) {
                         editor_place_menu.prev_category();
@@ -91,7 +129,7 @@ pub fn editor_step(memory: *api.GameMemory, inputs: *const Inputs, platform_api:
 
     if (inputs.a.pressed) {
         // open placement menu
-        editor_state.menu.push(.{ .editor_place = menus.EditorPlaceMenuState.init(make_placement_menu()) });
+        editor_state.menu.push(.{ .editor_place = menus.EditorPlaceMenuState.init(PLACEMENT_MENU) });
         return;
     }
 
@@ -100,9 +138,10 @@ pub fn editor_step(memory: *api.GameMemory, inputs: *const Inputs, platform_api:
     // TODO cursor movement
 }
 
-pub fn render_step(memory: *api.GameMemory, ctx: *api.RenderContext) callconv(.c) void {
+pub fn render_step(memory: *api.EditorMemory, ctx: *api.RenderContext) callconv(.c) void {
     const editor_state = memory.state;
     draw.fill_checkerboard(ctx.screen, 10, 0xFF, 0x00FF00);
+    render_shared.render_things(ctx.screen, ctx.storage, &editor_state.things);
     render_shared.render_menu(ctx.screen, ctx.storage, &editor_state.things, &editor_state.menu);
 }
 
